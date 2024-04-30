3f7c1ecea0588f6bfa90ad0504c5231bf9658531
feakh <akhmedzhanov.fechi@gmail.com>
2024-05-01 00:10:15 +0100

    comment

--- main.tf
+++ main.tf
@@ -9,3 +9,9 @@ resource "google_compute_network" "custom-test" {
   auto_create_subnetworks = false
   project      = "striped-reserve-419818"
 }
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.custom-test.id#returns the id of vpc created in line 7
+}
