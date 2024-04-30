161afd48113f91c3a81cdd2dfcf817ff9c1c33a3
feakh <akhmedzhanov.fechi@gmail.com>
2024-04-30 23:57:10 +0100

    comment

--- main.tf
+++ main.tf
@@ -7,4 +7,5 @@ resource "google_service_account" "service_account" {
 resource "google_compute_network" "custom-test" {
   name                    = "test-network"
   auto_create_subnetworks = false
+  project      = "striped-reserve-419818"
 }
