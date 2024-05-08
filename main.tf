965cfa5342b05a897b8650af4df9d2dc976442f6
feakh <153822098+feakh@users.noreply.github.com>
2024-05-08 17:26:48 +0100

    bucket names updated

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@
 
 resource "google_storage_bucket" "static" {
   for_each                    = toset(var.bucket_names)
-  name                        = "${each.key}"
+  name                        = each.key
   location                    = "US"
   storage_class               = "STANDARD"
   project                     = var.project_id
