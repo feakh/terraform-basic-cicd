6337436d61d331c6343b74a9479f2e3bf42b96e5
feakh <153822098+feakh@users.noreply.github.com>
2024-05-08 17:03:20 +0100

    comment out

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
-resource "google_service_account" "service_account" {
-  account_id   = "service-account-id"
-  display_name = var.env
-  project      = var.project_id
-}
+#resource "google_service_account" "service_account" {
+#  account_id   = "service-account-id"
+#  display_name = var.env
+#  project      = var.project_id
+#}
 # comment
-resource "google_compute_network" "custom-test" {
-  name                    = var.env
-  auto_create_subnetworks = false
-  project                 = var.project_id
-}
-resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
-  name          = var.env
-  ip_cidr_range = "10.2.0.0/16"
-  region        = var.region
-  project       = var.project_id
-  network       = google_compute_network.custom-test.id #returns the id of vpc created in line 7
-}
+#resource "google_compute_network" "custom-test" {
+#  name                    = var.env
+#  auto_create_subnetworks = false
+#  project                 = var.project_id
+#}
+#resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+#  name          = var.env
+#  ip_cidr_range = "10.2.0.0/16"
+#  region        = var.region
+#  project       = var.project_id
+#  network       = google_compute_network.custom-test.id #returns the id of vpc created in line 7
+#}
 
 resource "google_storage_bucket" "static" {
   count                       = 2
