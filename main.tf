07117994ab3c0e7d1967992149a8aab0e896d683
feakh <akhmedzhanov.fechi@gmail.com>
2024-05-11 23:40:08 +0100

    vm zone updatersksandigI

--- main.tf
+++ main.tf
@@ -85,7 +85,6 @@ resource "google_compute_instance" "my-devops-instance" {
   project = var.project_id
   boot_disk {
     auto_delete = true
-    #    device_name = "my-devops-instance"
 
 
     initialize_params {
@@ -94,16 +93,7 @@ resource "google_compute_instance" "my-devops-instance" {
       type  = "pd-balanced"
     }
 
-    #    mode = "READ_WRITE"
   }
-
-  #  can_ip_forward      = false
-  #  deletion_protection = false
-  #  enable_display      = false
-
-  #  labels = {
-  #    goog-ec-src = "vm_add-tf"
-  #  }
 
   machine_type = "e2-micro"
   name         = "my-devops-instance"
@@ -111,34 +101,12 @@ resource "google_compute_instance" "my-devops-instance" {
 
   network_interface {
     access_config {
-      network_tier = "STANDARD"
+      network_tier = "PREMIUM"
     }
 
-    nic_type    = "VIRTIO_NET"
-    queue_count = 0
-    stack_type  = "IPV4_ONLY"
     subnetwork  = "projects/striped-reserve-419818/regions/us-central1/subnetworks/subnet1"
   }
 
   allow_stopping_for_update = true
 
-  #  scheduling {
-  #    automatic_restart   = true
-  #    on_host_maintenance = "MIGRATE"
-  #    preemptible         = false
-  #    provisioning_model  = "STANDARD"
-  #  }
-
-  #  service_account {
-  #    email  = "235737959051-compute@developer.gserviceaccount.com"
-  #    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
-  #  }
-
-  #  shielded_instance_config {
-  #    enable_integrity_monitoring = true
-  #    enable_secure_boot          = false
-  #    enable_vtpm                 = true
-  #  }
-
-  #  zone = "us-central1-a"
 }
