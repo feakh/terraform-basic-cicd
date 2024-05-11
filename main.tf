6f9f58cb43eaf95858d61d5a3b3476c88ec68231
feakh <akhmedzhanov.fechi@gmail.com>
2024-05-11 23:05:10 +0100

    vm zone updatersks

--- main.tf
+++ main.tf
@@ -61,10 +61,10 @@ resource "google_storage_bucket" "static" {
 # Upload a text file as an object
 # to the storage bucket
 resource "google_storage_bucket_object" "default" {
- name         = "startup_file.txt"
- source       = "startup.sh"
- content_type = "text/plain"
- bucket       = google_storage_bucket.static.id
+  name         = "startup_file.txt"
+  source       = "startup.sh"
+  content_type = "text/plain"
+  bucket       = google_storage_bucket.static.id
 }
 
 # resource "google_storage_bucket" "static_loop" {
@@ -85,7 +85,7 @@ resource "google_compute_instance" "my-devops-instance" {
   boot_disk {
     auto_delete = true
     device_name = "my-devops-instance"
-    
+
 
     initialize_params {
       image = "projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20240508"
@@ -96,45 +96,46 @@ resource "google_compute_instance" "my-devops-instance" {
     mode = "READ_WRITE"
   }
 
-#  can_ip_forward      = false
-#  deletion_protection = false
-#  enable_display      = false
+  #  can_ip_forward      = false
+  #  deletion_protection = false
+  #  enable_display      = false
 
-#  labels = {
-#    goog-ec-src = "vm_add-tf"
-#  }
+  #  labels = {
+  #    goog-ec-src = "vm_add-tf"
+  #  }
 
   machine_type = "e2-medium"
   name         = "my-devops-instance"
+  zone         = "us-central1-a"
 
   network_interface {
     access_config {
       network_tier = "PREMIUM"
     }
 
-#    queue_count = 0
-#    stack_type  = "IPV4_ONLY"
-#    subnetwork  = "projects/striped-reserve-419818/regions/us-central1/subnetworks/subnet1"
-#  }
+    #    queue_count = 0
+    #    stack_type  = "IPV4_ONLY"
+    #    subnetwork  = "projects/striped-reserve-419818/regions/us-central1/subnetworks/subnet1"
+    #  }
 
-#  scheduling {
-#    automatic_restart   = true
-#    on_host_maintenance = "MIGRATE"
-#    preemptible         = false
-#    provisioning_model  = "STANDARD"
-#  }
+    #  scheduling {
+    #    automatic_restart   = true
+    #    on_host_maintenance = "MIGRATE"
+    #    preemptible         = false
+    #    provisioning_model  = "STANDARD"
+    #  }
 
-#  service_account {
-#    email  = "235737959051-compute@developer.gserviceaccount.com"
-#    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
-#  }
+    #  service_account {
+    #    email  = "235737959051-compute@developer.gserviceaccount.com"
+    #    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+    #  }
 
-#  shielded_instance_config {
-#    enable_integrity_monitoring = true
-#    enable_secure_boot          = false
-#    enable_vtpm                 = true
-#  }
+    #  shielded_instance_config {
+    #    enable_integrity_monitoring = true
+    #    enable_secure_boot          = false
+    #    enable_vtpm                 = true
+    #  }
 
-#  zone = "us-central1-a"
-}
+    #  zone = "us-central1-a"
+  }
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,19 +1,19 @@
-project_id   = "striped-reserve-419818"
-env          = "dev"
-region       = "us-central1"
+project_id = "striped-reserve-419818"
+env        = "dev"
+region     = "us-central1"
 # zone       = "us-central-1a"
 # bucket_names = ["fe-may8-2024-dev", "tst-may8-2024-ra", "ta-mya8-2024-prd"]
-bucket_names = "dev-may11-2024-fe"
+bucket_names   = "dev-may11-2024-fe"
 bucket_objects = "startup_file.txt"
-location     = ["US", "EU"]
+location       = ["US", "EU"]
 bucket_map = {
   may9-2024-fe-ma-test = "US"
   fe-pa-may9-2024-test = "EU"
 }
 
 subnetwork_map = {
-# name = cidr_range
-# key = "value"
+  # name = cidr_range
+  # key = "value"
   subnet1 = "10.20.30.0/24"
   subnet2 = "10.20.33.0/24"
   subnet3 = "10.20.36.0/24"
