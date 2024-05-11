0050ac94ed10e9873912693a4ab485e4da3f3abb
feakh <akhmedzhanov.fechi@gmail.com>
2024-05-12 00:11:38 +0100

    vm service accountss

--- main.tf
+++ main.tf
@@ -110,9 +110,8 @@ resource "google_compute_instance" "my-devops-instance" {
   allow_stopping_for_update = true
 
 service_account {
-  account_id   = "service-account-id"
-  display_name = var.env
-  project      = var.project_id
+  email   = "235737959051-compute@developer.gserviceaccount.com"
+  scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
 }
 
 }
