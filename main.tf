1f2fee76dd0c070e8dfe91cc70f3071ade00fa83
feakh <akhmedzhanov.fechi@gmail.com>
2024-05-11 23:32:12 +0100

    vm zone updatersksandig

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "google_compute_instance" "my-devops-instance" {
   project = var.project_id
   boot_disk {
     auto_delete = true
-#    device_name = "my-devops-instance"
+    #    device_name = "my-devops-instance"
 
 
     initialize_params {
@@ -94,7 +94,7 @@ resource "google_compute_instance" "my-devops-instance" {
       type  = "pd-balanced"
     }
 
-#    mode = "READ_WRITE"
+    #    mode = "READ_WRITE"
   }
 
   #  can_ip_forward      = false
@@ -114,29 +114,31 @@ resource "google_compute_instance" "my-devops-instance" {
       network_tier = "STANDARD"
     }
 
-        nic_type    = "VIRTIO_NET"
-        queue_count = 0
-        stack_type  = "IPV4_ONLY"
-        subnetwork  = "projects/striped-reserve-419818/regions/us-central1/subnetworks/subnet1"
-      }
-
-    #  scheduling {
-    #    automatic_restart   = true
-    #    on_host_maintenance = "MIGRATE"
-    #    preemptible         = false
-    #    provisioning_model  = "STANDARD"
-    #  }
-
-    #  service_account {
-    #    email  = "235737959051-compute@developer.gserviceaccount.com"
-    #    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
-    #  }
-
-    #  shielded_instance_config {
-    #    enable_integrity_monitoring = true
-    #    enable_secure_boot          = false
-    #    enable_vtpm                 = true
-    #  }
-
-    #  zone = "us-central1-a"
+    nic_type    = "VIRTIO_NET"
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/striped-reserve-419818/regions/us-central1/subnetworks/subnet1"
   }
+
+  allow_stopping_for_update = true
+
+  #  scheduling {
+  #    automatic_restart   = true
+  #    on_host_maintenance = "MIGRATE"
+  #    preemptible         = false
+  #    provisioning_model  = "STANDARD"
+  #  }
+
+  #  service_account {
+  #    email  = "235737959051-compute@developer.gserviceaccount.com"
+  #    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  #  }
+
+  #  shielded_instance_config {
+  #    enable_integrity_monitoring = true
+  #    enable_secure_boot          = false
+  #    enable_vtpm                 = true
+  #  }
+
+  #  zone = "us-central1-a"
+}
