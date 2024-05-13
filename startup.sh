b7a35888ea258be205d3d351515b6486225d1e56
feakh <akhmedzhanov.fechi@gmail.com>
2024-05-14 00:38:28 +0100

    update startup script apache

--- startup.sh
+++ startup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-yum update
+# Update package lists
+apt-get update
+
+# Install Apache
+apt-get install -y apache2
+
+# Start Apache
+systemctl start apache2
+
+# Enable Apache to start on boot
+systemctl enable apache2
