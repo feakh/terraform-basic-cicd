2df3af4cb78a7ec3835143fdbdd6bd2a45835653
feakh <akhmedzhanov.fechi@gmail.com>
2024-05-14 02:02:36 +0100

    update startup script apache update

--- startup.sh
+++ startup.sh
@@ -6,8 +6,8 @@ apt-get update
 # Install Apache
 apt-get install -y apache2
 
-# Start Apache
-systemctl start apache2
-
 # Enable Apache to start on boot
 systemctl enable apache2
+
+# Start Apache
+systemctl start apache2
