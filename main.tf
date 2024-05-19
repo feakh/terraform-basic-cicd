3a99f5724cdafaf7c99208ed1a32b7898caeaf8e
feakh <akhmedzhanov.fechi@gmail.com>
2024-05-19 19:16:03 +0100

    upd

--- main.tf
+++ main.tf
@@ -260,8 +260,8 @@ resource "google_compute_instance_group" "unmanaged_instance_group" {
   #instance_names = ["instance-1", "instance-2"]
 
   instances = [
-    "google_compute_instance.my-devops-instance-0.instance.name",
-    "google_compute_instance.my-devops-instance-1.instance.name",
+    google_compute_instance.my-devops-instance-0.instance.name,
+    google_compute_instance.my-devops-instance-1.instance.name,
   ]
 
   #instances = [
