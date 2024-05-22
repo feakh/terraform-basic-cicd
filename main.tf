e1d9a7cbabc6333140accc8c03c8aa85173a2e3e
feakh <akhmedzhanov.fechi@gmail.com>
2024-05-23 00:49:25 +0100

    load balanced

--- main.tf
+++ main.tf
@@ -298,6 +298,7 @@ resource "google_compute_backend_service" "default" {
   protocol              = "HTTP"
   enable_cdn  = false
   timeout_sec = 30
+  load_balancing_scheme = "EXTERNAL"
   
 
 #  name                  = "tf-test-backend-service-external"
