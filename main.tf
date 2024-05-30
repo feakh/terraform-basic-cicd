a119076c08b2035a0ba021b83daa99dc31fcad15
feakh <akhmedzhanov.fechi@gmail.com>
2024-05-30 18:47:23 +0100

    backendservicedlfjjn

--- main.tf
+++ main.tf
@@ -288,6 +288,7 @@ resource "google_compute_global_address" "default" {
 resource "google_compute_target_http_proxy" "default" {
   name    = "http-lb-proxy"
   url_map = google_compute_url_map.default.id
+  project     = var.project_id
 }
 
 resource "google_compute_url_map" "default" {
