20ff5be8eefc03e354c946c1c0f083b99a656e8b
feakh <akhmedzhanov.fechi@gmail.com>
2024-05-30 17:18:19 +0100

    backendellkpp

--- main.tf
+++ main.tf
@@ -260,7 +260,6 @@ resource "google_compute_instance_group" "unmanaged_instance_group" {
   #instance_names = ["instance-1", "instance-2"]
 
   instances = [
-    google_compute_instance.my-devops-instance[0].self_link,
     google_compute_instance.my-devops-instance[1].self_link
     #  google_compute_instance.my-devops-instance-1.instance.name,
   ]
